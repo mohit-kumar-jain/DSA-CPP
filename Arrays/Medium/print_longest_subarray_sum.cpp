09d034bf5f7b3872623beb005956ae73f08d0561
mohit-kumar-jain <mohitff56@gmail.com>
2026-06-09 22:40:06 +0530

    DAY - 11

--- Arrays/Medium/print_longest_subarray_sum.cpp
+++ Arrays/Medium/print_longest_subarray_sum.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Brute.T.C -> O(n*n) ,S.C -> O(1).
+// void printMaxSubarrayBrute(int arr[], int n)
+// {
+//     int maxxi = INT_MIN;
+//     int Arrstart = 0, Arrend = 0;
+//     for(int i = 0; i < n; i++)
+//     {
+//         int sum = 0;
+//         for(int j = i; j < n; j++)
+//         {
+//             sum += arr[j];
+//             if(sum > maxxi)
+//             {
+//                 maxxi = sum;
+//                 Arrstart = i;
+//                 Arrend = j;
+//             }
+//         }
+//     }
+//     for(int i = Arrstart; i <= Arrend; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
 // Optimal.T.C -> O(n) ,S.C -> O(1).
 void printLongestSubarraySum(int arr[],int n){
     int maxxi = INT_MIN,sum =0;
