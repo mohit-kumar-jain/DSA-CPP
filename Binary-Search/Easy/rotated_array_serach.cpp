d057242676e7f94819f032b84467484c735e960a
mohit-kumar-jain <mohitff56@gmail.com>
2026-06-17 22:20:37 +0530

    DAY - 19

---
+++ Binary-Search/Easy/rotated_array_serach.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute. T.C -> O(n), S.C -> O(1).
+// int rotateSearch(vector<int>& nums,int t){
+//     int n = nums.size();
+//     for (int i = 0; i < n; i++)
+//     {
+//         if(nums[i] == t){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// Optimal. T.C -> O(logn), S.C -> O(1).
+int rotateSearch(vector<int> &nums, int t)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] == t)
+        {
+            return mid;
+        }
+        else if (nums[low] <= nums[mid])
+        { // Left half sorted
+            if (nums[low] <= t && t < nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        { // Right half sorted
+            if (nums[mid] < t && t <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {7, 8, 9, 1, 2, 3};
+    int target;
+    cin >> target;
+    cout << rotateSearch(arr, target) << endl;
+    return 0;
+}
