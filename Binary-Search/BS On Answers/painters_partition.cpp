cd4f07ca51ee5d69c72640a02cff7f32f4c70b98
mohit-kumar-jain <mohitff56@gmail.com>
2026-06-20 00:07:33 +0530

    DAY - 22

--- Binary-Search/BS On Answers/painters_partition.cpp
+++ Binary-Search/BS On Answers/painters_partition.cpp
@@ -3,22 +3,21 @@ using namespace std;
 
 // Brute. T.C -> O(N * (sum-max+1)), S.C -> O(1).
 
-// bool isPossible(vector<int>& nums, int numbers, int k){
-//     int n = nums.size();
-//     int sum = 0,cnt = 1;
-//     for (int i = 0; i < n; i++)
+// int isPossible(vector<int>& nums, int paint, int k){
+//     int count = 1,sum = 0;
+//     for (int i = 0; i < nums.size(); i++)
 //     {
-//         if(nums[i] + sum <= numbers){
+//         if(nums[i] + sum <= paint){
 //             sum += nums[i];
 //         }else{
-//             cnt++;
+//             count++;
 //             sum = nums[i];
 //         }
-//     }
-//     return cnt <= k;
+//     }  
+//     return count <= k;
 // }
 
-// int spilitArray(vector<int>& nums,int k){
+// int partition(vector<int>& nums, int k){
 //     if(k > nums.size()) return -1;
 //     int low = *max_element(nums.begin(),nums.end());
 //     int high = accumulate(nums.begin(),nums.end(),0);
@@ -26,36 +25,34 @@ using namespace std;
 //     {
 //         if(isPossible(nums,i,k)){
 //             return i;
-//         }
+//         }   
 //     }
 //     return -1;
 // }
 
-// Optimal. T.C -> O(N * (log(sum-max+1)), S.C -> O(1).
+// Optimal. T.C -> O(N * log(sum-max+1)), S.C -> O(1).
 
-bool isPossible(vector<int>& nums, int numbers, int k){
-    int n = nums.size();
-    int sum = 0,cnt = 1;
-    for (int i = 0; i < n; i++)
+int isPossible(vector<int>& nums, int paint, int k){
+    int count = 1,sum = 0;
+    for (int i = 0; i < nums.size(); i++)
     {
-        if(nums[i] + sum <= numbers){
+        if(nums[i] + sum <= paint){
             sum += nums[i];
         }else{
-            cnt++;
+            count++;
             sum = nums[i];
         }
-    }
-    return cnt <= k;
+    }  
+    return count <= k;
 }
 
-int spilitArray(vector<int>& nums,int k){
+int partition(vector<int>& nums, int k){
     if(k > nums.size()) return -1;
     int low = *max_element(nums.begin(),nums.end());
     int high = accumulate(nums.begin(),nums.end(),0);
     while(low <= high){
         int mid = low + (high - low) / 2;
-        if (isPossible(nums,mid,k))
-        {
+        if(isPossible(nums,mid,k)){
             high = mid - 1;
         }else{
             low = mid + 1;
@@ -63,10 +60,11 @@ int spilitArray(vector<int>& nums,int k){
     }
     return low;
 }
+
 int main()
 {
-    vector<int> arr = {1,2,3,4,5};
-    int k = 3;
-    cout << spilitArray(arr,k);
+    vector<int> arr = {10, 20, 30, 40};
+    int k = 2;
+    cout << partition(arr,k);
     return 0;
 }
