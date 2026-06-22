402e6dbef616f714bbf3004e482eb1af23946597
mohit-kumar-jain <mohitff56@gmail.com>
2026-06-22 22:46:21 +0530

    DAY - 24

---
+++ Strings/Basic and Easy/sort_vowels.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>         
+using namespace std;
+
+// Optimal. T.C. -> O(n), S.C. -> O(no.of.vowels).
+string sortVowels(string& s){
+    string res;
+    vector<int> lower(26,0); 
+    vector<int> upper(26,0); 
+    for (int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == 'a'||s[i] == 'e'||s[i] == 'o'||s[i] == 'u'||s[i] == 'i'){
+            lower[s[i] - 'a']++;
+            s[i] = '$';
+        }
+        else if(s[i] == 'A'||s[i] == 'E'||s[i] == 'I'||s[i] == 'O'||s[i] == 'U'){
+            upper[s[i] - 'A']++;
+            s[i] = '$';
+        }
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        char c = 'A' + i;
+        while(upper[i]){
+            res += c;
+            upper[i]--;
+        }
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        char c = 'a' + i;
+        while(lower[i]){
+            res += c;
+            lower[i]--;
+        }
+    }
+    int first = 0, second  = 0;
+    while(first < s.size() && second < res.size()){
+        if(s[first] == '$'){
+            s[first] = res[second];
+            second++;
+        }
+        first++;
+    }
+    
+    return s;
+}
+
+int main() {
+    string s = "lEetcOde";
+    string res = sortVowels(s);
+    cout << res << endl;
+    return 0;
+}
