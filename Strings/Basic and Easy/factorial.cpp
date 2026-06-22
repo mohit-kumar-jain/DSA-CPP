8ccceaf4eefe4dcf09348f9a0ee5de8d6ee85a04
mohit-kumar-jain <mohitff56@gmail.com>
2026-06-23 02:07:48 +0530

    DAY - 24

---
+++ Strings/Basic and Easy/factorial.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Optimal. T.C. -> O(1), S.C. -> O(1).
+vector<int> factorial(int num)
+{
+    vector<int> res(1,1);
+    while(num > 1){
+        int sum,carry = 0,size = res.size();
+        for (int i = 0; i < size; i++)
+        {
+            sum = res[i] * num + carry;
+            res[i] = sum % 10;
+            carry = sum / 10;
+        }
+        while(carry){
+            res.push_back(carry % 10);
+            carry /= 10;
+        }
+        num--;
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
+
+int main()
+{
+    int num = 5;
+    vector<int> res = factorial(num);
+    for(auto it : res){
+        cout << it << "";
+    }
+    return 0;
+}
