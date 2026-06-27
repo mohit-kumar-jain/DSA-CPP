6d536df353d9eefc1f2f7e41a5ca8e07e4fd9dd4
mohit-kumar-jain <mohitff56@gmail.com>
2026-06-27 15:55:32 +0530

    DAY - 28

--- Strings/Basic and Easy/roman_to_integer.cpp
+++ Strings/Basic and Easy/roman_to_integer.cpp
@@ -1,38 +1,26 @@
-#include<bits/stdc++.h>       
+#include <bits/stdc++.h>
 using namespace std;
 
 // Optimal. T.C. -> O(n), S.C. -> O(1).
-
-int roman(char c) {
-    switch (c) {
-        case 'I': return 1;
-        case 'V': return 5;
-        case 'X': return 10;
-        case 'L': return 50;
-        case 'C': return 100;
-        case 'D': return 500;
-        case 'M': return 1000;
+int romanToInteger(string s)
+{
+    int res = 0;
+    unordered_map<char, int> roman = {
+        {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+         {'C', 100}, {'D', 500}, {'M', 1000}
+    };
+    for (int i = 0; i < s.size() - 1; i++)
+    {
+        if (roman[s[i]] < roman[s[i + 1]])
+            res -= roman[s[i]];
+        else
+            res += roman[s[i]];
     }
-    return 0;
+    return res + roman[s.back()];
 }
 
-int romanToInteger(string s){
-    int sum = 0,index = 0;
-    while(index < s.size() - 1){
-        if(roman(s[index]) < roman(s[index + 1])){
-            sum -= roman(s[index]);
-            index++;
-        }else{
-            sum += roman(s[index]);
-            index++;
-        }
-    }
-    sum += roman(s[index]);
-
-    return sum;
-}
-
-int main() {
+int main()
+{
     string s = "LXXX";
     cout << romanToInteger(s) << endl;
     return 0;
