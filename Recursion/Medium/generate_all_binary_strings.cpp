bf0fafb0844fc591a000c57e638b3f1df7cabc13
mohit-kumar-jain <mohitff56@gmail.com>
2026-07-18 23:49:25 +0530

    DAY - 46

---
+++ Recursion/Medium/generate_all_binary_strings.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// T.C -> O(2^N), S.C -> O(N)
+void generate(int n, string curr, vector<string>& result) {
+    if (curr.length() == n) {
+        result.push_back(curr);
+        return;
+    }
+    generate(n, curr + "0", result);
+    if (curr.empty() || curr.back() != '1') {
+        generate(n, curr + "1", result);
+    }
+}
+
+int main() {
+    int n = 3;
+    vector<string> result;
+    generate(n, "", result);
+    for (string& s : result) {
+        cout << s << " ";
+    }
+    cout << endl;
+    return 0;
+}
