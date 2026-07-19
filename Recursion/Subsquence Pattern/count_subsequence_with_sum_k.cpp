eb319ae9a2906e30ef09aabae9876f8c9fded1aa
mohit-kumar-jain <mohitff56@gmail.com>
2026-07-19 23:38:10 +0530

    DAY - 47

---
+++ Recursion/Subsquence Pattern/count_subsequence_with_sum_k.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// T.C -> O(2^n), S.C -> O(N).
+int func(int ind, int sum, vector<int> &nums)
+{
+    if (sum == 0)
+        return 1;
+    if (sum < 0 || ind == nums.size())
+        return 0;
+    return func(ind + 1, sum - nums[ind], nums) + func(ind + 1, sum, nums);
+}
+int countSubsequenceWithTargetSum(vector<int> &nums, int target)
+{
+    return func(0, target, nums);
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5};
+    int target = 5;
+    cout << "Number of subsequences with target sum " << target << ": "
+         << countSubsequenceWithTargetSum(nums, target) << endl;
+    return 0;
+}
