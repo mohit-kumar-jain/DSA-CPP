5bf37aa83ce46a52e120f7c88ce2b84253c0ad88
mohit-kumar-jain <mohitff56@gmail.com>
2026-07-23 23:55:09 +0530

    DAY - 51

---
+++ Loops/reverse.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int reverse(int x)
+{
+    int ans = 0;
+    while (x != 0)
+    {
+        int digit = x % 10;
+
+        if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10))
+        {
+            return 0;
+        }
+
+        ans = ans * 10 + digit;
+        x = x / 10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 125;
+    cout << n << endl;
+    cout << reverse(n) << endl;
+    return 0;
+}
