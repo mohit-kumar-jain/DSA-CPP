0a26ca9b83132f6b15500c22f81c560e91288474
mohit-kumar-jain <mohitff56@gmail.com>
2026-08-15 20:29:32 +0530

    DAY - 64

---
+++ Arrays/Hard/Container_with_most_water.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxArea(vector<int> &height)
+{
+    int maxArea = 0;
+    int left = 0;
+    int right = height.size() - 1;
+
+    while (left < right)
+    {
+        maxArea = max(maxArea, (right - left) * min(height[left], height[right]));
+
+        if (height[left] < height[right])
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+    return maxArea;
+}
+
+int main()
+{
+    vector<int> heights = {1,8,6,2,5,4,8,3,7};
+    cout << maxArea(heights) << endl;
+    return 0;
+}
