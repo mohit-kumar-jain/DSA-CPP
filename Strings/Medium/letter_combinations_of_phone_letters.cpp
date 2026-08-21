a114ff67e5b7f79e21a677663c93eaf65e9b117d
mohit-kumar-jain <mohitff56@gmail.com>
2026-08-21 18:22:44 +0530

    Gay - 68

---
+++ Strings/Medium/letter_combinations_of_phone_letters.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void backtrack(const string &digits, int idx, string comb,
+               vector<string> &res,
+               const unordered_map<char, string> &digitToLetters)
+{
+    if (idx == digits.length())
+    {
+        res.push_back(comb);
+        return;
+    }
+
+    string letters = digitToLetters.at(digits[idx]);
+    for (char letter : letters)
+    {
+        backtrack(digits, idx + 1, comb + letter, res, digitToLetters);
+    }
+}
+
+vector<string> letterCombinations(string digits)
+{
+    vector<string> res;
+
+    if (digits.empty())
+    {
+        return res;
+    }
+
+    unordered_map<char, string> digitToLetters = {
+        {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+
+    backtrack(digits, 0, "", res, digitToLetters);
+
+    return res;
+}
+
+
+int main()
+{
+    string s = "2";
+    vector<string> res = letterCombinations(s);
+    for(auto ch : res) cout << ch << " ";
+    cout << endl;
+    return 0;
+}
