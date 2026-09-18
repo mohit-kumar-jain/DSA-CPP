79aa5b2463ba48a41c6d90be8c997d586abb0317
mohit-kumar-jain <mohitff56@gmail.com>
2026-09-18 22:46:38 +0530

    DAY - 89

---
+++ Bit - Manipulation/Medium/Minimum_Bit_Flips_to_Convert_Number.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minBitFlips(int start, int goal)
+{
+    int xorResult = start ^ goal;
+    int ans = 0;
+    while (xorResult > 0)
+    {
+        ans += xorResult & 1;
+        xorResult >>= 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    int start = 10, goal = 5;
+    cout << minBitFlips(start, goal) << endl;
+    return 0;
+}
